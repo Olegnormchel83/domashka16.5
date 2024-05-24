b50fe17e44245f29652e0e71e5e7ad1f9569a21d
Olegnormchel83 <cranberries2000@gmail.com>
2024-05-24 20:49:13 +0300

    Добавьте файлы проекта.

---
+++ domashka16.5.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <time.h>
+
+int main()
+{
+    struct tm buf;
+    time_t t = time(NULL);
+    localtime_s(&buf, &t);
+    int Today = buf.tm_mday;
+    const int SIZE = 5;
+    int MyArray[SIZE][SIZE];
+    int TargetRow = Today / SIZE;
+    int sum = 0;
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            MyArray[i][j] = i + j;
+            std::cout << MyArray[i][j] << ' ';
+
+            if (i == TargetRow)
+            {
+                sum += MyArray[i][j];
+            }
+        }
+        std::cout << '\n';
+    }
+
+    std::cout << '\n';
+    std::cout << "Today is the " << Today << "th\n";
+    std::cout << "Array size is " << SIZE << '\n';
+    std::cout << "It means we need " << TargetRow << " row\n";
+    std::cout << "The sum of " << TargetRow << " row: " << sum << '\n';
+
+    return 0;
+}
